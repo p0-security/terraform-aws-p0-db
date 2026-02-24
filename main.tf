e8c88e0572f50378c9ca640ddcedab7eb2d0f89b
Brendan Berrigan <bgberr@gmail.com>
2026-02-24 06:11:43 -0800

    Add Security Groups (#3)
    * Fix Security Groups
    
    * Changing to data source for lambda
    
    * Getting from vars again instead of data sources
    
    * Update variables.tf
    
    Co-authored-by: Nathan Brahms <nbrahms@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Brendan Berrigan <brendan.berrigan@p0.dev>
    Co-authored-by: Nathan Brahms <nbrahms@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -2,18 +2,55 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
 
-data "aws_caller_identity" "current" {}
+locals {
+  is_cluster     = split(":", var.rds_arn)[5] == "cluster"
+  aws_account_id = split(":", var.rds_arn)[4]
+  aws_region     = split(":", var.rds_arn)[3]
+  aws_partition  = split(":", var.rds_arn)[1]
+}
 
-data "aws_region" "current" {}
+data "aws_rds_cluster" "database" {
+  region             = local.aws_region
+  count              = local.is_cluster ? 1 : 0
+  cluster_identifier = reverse(split(":", var.rds_arn))[0]
+}
+
+data "aws_db_instance" "database" {
+  region                 = local.aws_region
+  count                  = local.is_cluster ? 0 : 1
+  db_instance_identifier = reverse(split(":", var.rds_arn))[0]
+}
 
 locals {
-  aws_account_id = coalesce(var.aws_account_id, data.aws_caller_identity.current.account_id)
-  aws_region     = coalesce(var.aws_region, data.aws_region.current.id)
+  database_security_group_id  = local.is_cluster ? tolist(data.aws_rds_cluster.database[0].vpc_security_group_ids)[0] : tolist(data.aws_db_instance.database[0].vpc_security_groups)[0].id
+  port                        = local.is_cluster ? data.aws_rds_cluster.database[0].port : data.aws_db_instance.database[0].port
+  resource_id                 = local.is_cluster ? data.aws_rds_cluster.database[0].cluster_resource_id : data.aws_db_instance.database[0].resource_id
+}
+
+# Security group rules allowing connector to access database
+resource "aws_security_group_rule" "connector_to_database" {
+  type                     = "egress"
+  description              = "Outbound to database"
+  from_port                = local.port
+  to_port                  = local.port
+  protocol                 = "tcp"
+  security_group_id        = var.connector_security_group_id
+  source_security_group_id = local.database_security_group_id
+}
+
+resource "aws_security_group_rule" "database_from_connector" {
+  type                     = "ingress"
+  description              = "Database traffic from P0 connector Lambda"
+  from_port                = local.port
+  to_port                  = local.port
+  protocol                 = "tcp"
+  security_group_id        = local.database_security_group_id
+  source_security_group_id = var.connector_security_group_id
 }
 
 # RDS describe and connect policy for Lambda
@@ -31,7 +68,7 @@ resource "aws_iam_role_policy" "lambda_rds_describe" {
           "rds-db:connect"
         ]
         Resource = [
-          "arn:aws:rds-db:${local.aws_region}:${local.aws_account_id}:dbuser:${var.rds_resource_id}/p0_iam_manager"
+          "arn:${local.aws_partition}:rds-db:${local.aws_region}:${local.aws_account_id}:dbuser:${local.resource_id}/p0_iam_manager"
         ]
       }
     ]
--- variables.tf
+++ variables.tf
@@ -1,26 +1,3 @@
-variable "arch" {
-  description = "Database architecture"
-  type        = string
-  validation {
-    condition     = contains(["mysql", "pg"], var.arch)
-    error_message = "'arch' must be one of 'mysql' or 'pg'"
-  }
-}
-
-variable "aws_account_id" {
-  description = "The AWS account ID"
-  nullable    = true
-  default     = null
-  type        = string
-}
-
-variable "aws_region" {
-  description = "The AWS region"
-  nullable    = true
-  default     = null
-  type        = string
-}
-
 variable "lambda_execution_role_name" {
   description = "Name of the connector Lambda function's service role"
   type        = string
@@ -31,7 +8,7 @@ variable "rds_arn" {
   type        = string
 }
 
-variable "rds_resource_id" {
-  description = "RDS instance or cluster resource ID"
+variable "connector_security_group_id" {
+  description = "ID of the P0 connector Lambda's security group."
   type        = string
 }
